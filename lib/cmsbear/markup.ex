6ef885a926a840b03521463ca057cf232c3e5bcf
Jói Sigurdsson <joi@crankwheel.com>
2022-06-24 09:25:14 +0000

    Start work of processing images in Markdown first with link to metadata in pre; next step is to process AST with those traits.

--- lib/cmsbear/markup.ex
+++ lib/cmsbear/markup.ex
@@ -32,42 +32,49 @@ defmodule Cmsbear.Markup do
     |> Enum.join(" ")
   end
 
-  def generate_image_html(path, image_opts) do
-    image_attribs = generate_image_attribs(image_opts)
-    image = "<img src='/bimg/#{path}' #{image_attribs} />"
-    "<p>" <>
-    case Map.get(image_opts, "figcaption", nil) do
-      nil ->
-        image
-      caption ->
-        id = UUID.uuid4()
-        "<figure aria-describedby='#{id}'>#{image}<figcaption id='#{id}'>#{caption}</figcaption></figure>"
+  def generate_image_html(path, metadata_section \\ "") do
+    {title_part, metadata_section} = case metadata_section do
+      "" ->
+        {"", ""}
+      _ ->
+        link_id = UUID.uuid4()
+        {
+          " \"#{link_id}\"",
+          String.replace(metadata_section, "```cmsbear-metadata", "```cmsbear-metadata-#{link_id}")
+        }
     end
-    <> "</p>"
+    "![image](/bimg/#{path}#{title_part})\n#{metadata_section}"
+
+    #case Map.get(image_opts, "figcaption", nil) do
+    #  nil ->
+    #    image
+    #  caption ->
+    #    id = UUID.uuid4()
+    #    "<figure aria-describedby='#{id}'>#{image}<figcaption id='#{id}'>#{caption}</figcaption></figure>"
+    #end
   end
 
   def replace_image_links(html) do
-    Regex.replace(~r/\[image:([^]]+)\]\s*(```cmsbear-image.*?```)?/s, html, fn (all, path, image_section) ->
-      case image_section do
-        "" -> generate_image_html(path, %{})
-        _ -> generate_image_html(path, get_kv_section(image_section, "cmsbear-image"))
+    Regex.replace(~r/\[image:([^]]+)\]\s*(```cmsbear-metadata.*?```)?/s, html, fn (all, path, metadata_section) ->
+      case metadata_section do
+        "" -> generate_image_html(path)
+        _ -> generate_image_html(path, metadata_section)
       end
-      <> "\n"  # Magic linebreak so Earmark doesn't discard the rest of the line
     end)
   end
 
   def replace_file_links(html) do
-    Regex.replace(~r/\[file:([^\/]+\/([^]]+))]/, html, fn _, path, filename -> "<a href='/bfile/#{path}'>#{filename}</a>\n" end)
+    Regex.replace(~r/\[file:([^\/]+\/([^]]+))]/, html, fn _, path, filename -> "Download: [#{filename}](/bfile/#{path})" end)
   end
 
   def note_to_html(note) do
     note = note
+    |> replace_image_links()
+    |> replace_file_links()
     |> without_tags()
     {:ok, ast, _} = EarmarkParser.as_ast(note)
     ast
-    |> IO.inspect()
     |> process_ast()
-    |> IO.inspect()
     |> Earmark.Transform.transform()
   end
 
@@ -79,26 +86,12 @@ defmodule Cmsbear.Markup do
     Regex.scan(~r/\#[a-z0-9-_\/]+/, note) |> Enum.flat_map(&(&1))
   end
 
-  # We process the ast to replace "em" with "strong" (because "Bear *really* likes fish",
-  # and to find matching pairs of word-adjacent slashes and change to italic (because
-  # Bear "has a certain /je ne c'est quoi/ to it").
+  # We process the ast to replace "em" with "strong" (because "Bear *boldly* goes
+  # where no bear has gone before", and to find matching pairs of word-adjacent
+  # slashes and change to italic (because "Bear has a certain /je ne c'est quoi/
+  # to it").
   def process_ast(items) do
     Enum.map(items, fn item -> process_ast_item(item) end)
-  end
-
-  def process_ast_item_splitting_regex(item, regex, map_captures_fn)
-  when is_binary(item) and is_function(map_captures_fn) do
-    interest_parts = Regex.scan(regex, item)
-    |> Enum.map(map_captures_fn)
-    other_parts = Regex.split(regex, item)
-    case Regex.scan(regex, item, return: :index) do
-      [[{0, _}|_rest]|_rest2] ->
-        zigzag_lists(interest_parts, other_parts)
-      [_|_] ->
-        zigzag_lists(other_parts, interest_parts)
-      _ ->
-        item
-    end
   end
 
   def process_ast_item(item) when is_binary(item) do
@@ -116,6 +109,21 @@ defmodule Cmsbear.Markup do
   def process_ast_item({type, attribs, items, annotations})
   when is_binary(type) and is_list(attribs) and is_list(items) and is_map(annotations) do
     {type, attribs, List.flatten(process_ast(items)), annotations}
+  end
+
+  def process_ast_item_splitting_regex(item, regex, map_captures_fn)
+  when is_binary(item) and is_function(map_captures_fn) do
+    interest_parts = Regex.scan(regex, item)
+    |> Enum.map(map_captures_fn)
+    other_parts = Regex.split(regex, item)
+    case Regex.scan(regex, item, return: :index) do
+      [[{0, _}|_rest]|_rest2] ->
+        zigzag_lists(interest_parts, other_parts)
+      [_|_] ->
+        zigzag_lists(other_parts, interest_parts)
+      _ ->
+        item
+    end
   end
 
   def zigzag_lists(first, second, acc \\ [])
