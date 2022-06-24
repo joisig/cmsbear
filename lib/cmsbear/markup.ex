6bd15afbee300517faa6f7a07f5c4254458927fb
Jói Sigurdsson <joi@crankwheel.com>
2022-06-24 13:11:38 +0000

    Complete transition to handling images by fixing up markup, then referring to metadata when munging AST

--- lib/cmsbear/markup.ex
+++ lib/cmsbear/markup.ex
@@ -1,13 +1,9 @@
 defmodule Cmsbear.Markup do
 
-  def get_kv_section(text, section_id) do
-    front_matter = case Regex.named_captures(~r/^.*?```\n?(#{section_id})\n(?<lines>.*?)```.*?$/s, text) do
-      nil ->
-        []
-      %{"lines" => lines} ->
-        String.split(lines, "\n")
-    end
-    |> Enum.flat_map(fn line ->
+  @default_image_opts %{"attrib-max-width" => "100%"}
+
+  def kv_lines_to_map(kv_lines) do
+    Enum.flat_map(kv_lines, fn line ->
       case Regex.named_captures(~r/\s*(?<key>[a-zA-Z0-9-_]+):\s*(?<val>.*)/, line) do
         nil ->
           []
@@ -18,21 +14,17 @@ defmodule Cmsbear.Markup do
     |> Enum.into(%{})
   end
 
-  def generate_image_attribs(image_opts) do
-    attribs = Enum.flat_map(
-      image_opts |> Enum.into(%{"attrib-max-width" => "100%"}),
-      fn {key, val} ->
-        case key do
-          "attrib-" <> attrib_name -> [{attrib_name, val}]
-          "alt" -> [{"alt", val}]
-          _ -> []
-        end
-      end)
-    |> Enum.map(fn {key, val} -> "#{key}='#{val}'" end)
-    |> Enum.join(" ")
+  def get_kv_section(text, section_id) do
+    front_matter = case Regex.named_captures(~r/^.*?```\n?(#{section_id})\n(?<lines>.*?)```.*?$/s, text) do
+      nil ->
+        []
+      %{"lines" => lines} ->
+        String.split(lines, "\n")
+    end
+    |> kv_lines_to_map()
   end
 
-  def generate_image_html(path, metadata_section \\ "") do
+  def fixup_image_markup_impl(path, metadata_section \\ "") do
     {title_part, metadata_section} = case metadata_section do
       "" ->
         {"", ""}
@@ -44,73 +36,163 @@ defmodule Cmsbear.Markup do
         }
     end
     "![image](/bimg/#{path}#{title_part})\n#{metadata_section}"
-
-    #case Map.get(image_opts, "figcaption", nil) do
-    #  nil ->
-    #    image
-    #  caption ->
-    #    id = UUID.uuid4()
-    #    "<figure aria-describedby='#{id}'>#{image}<figcaption id='#{id}'>#{caption}</figcaption></figure>"
-    #end
   end
 
-  def replace_image_links(html) do
+  def fixup_image_markup(html) do
     Regex.replace(~r/\[image:([^]]+)\]\s*(```cmsbear-metadata.*?```)?/s, html, fn (all, path, metadata_section) ->
       case metadata_section do
-        "" -> generate_image_html(path)
-        _ -> generate_image_html(path, metadata_section)
+        "" -> fixup_image_markup_impl(path)
+        _ -> fixup_image_markup_impl(path, metadata_section)
       end
     end)
   end
 
-  def replace_file_links(html) do
+  def fixup_file_markup(html) do
     Regex.replace(~r/\[file:([^\/]+\/([^]]+))]/, html, fn _, path, filename -> "Download: [#{filename}](/bfile/#{path})" end)
   end
 
   def note_to_html(note) do
     note = note
-    |> replace_image_links()
-    |> replace_file_links()
-    |> without_tags()
+    |> fixup_image_markup()
+    |> fixup_file_markup()
+    |> strip_tags()
     {:ok, ast, _} = EarmarkParser.as_ast(note)
+    {ast, metadata_sections} = split_metadata_sections(ast)
     ast
-    |> process_ast()
+    |> handle_bold()
+    |> handle_italics()
+    |> handle_images_and_figures(metadata_sections)
     |> Earmark.Transform.transform()
   end
 
-  def without_tags(note) do
+  def strip_tags(note) do
     Regex.replace(~r/\#[a-zA-Z][a-z0-9-_\/]+/, note, "")
   end
 
   def tags(note) do
-    Regex.scan(~r/\#[a-z0-9-_\/]+/, note) |> Enum.flat_map(&(&1))
+    Regex.scan(~r/\#[a-z0-9-_\/]+/, note) |> Enum.flat_map(&(&1)) |> Enum.uniq()
   end
 
   # We process the ast to replace "em" with "strong" (because "Bear *boldly* goes
   # where no bear has gone before", and to find matching pairs of word-adjacent
   # slashes and change to italic (because "Bear has a certain /je ne c'est quoi/
   # to it").
-  def process_ast(items) do
-    Enum.map(items, fn item -> process_ast_item(item) end)
+  def handle_italics(ast) do
+    ast
+    |> walk_and_modify_ast(0, &handle_italics_impl/2)
+    |> elem(0)
   end
-
-  def process_ast_item(item) when is_binary(item) do
-    text_to_ast_list_splitting_regex(
+  def handle_italics_impl(item, acc) when is_binary(item) do
+    new_item = text_to_ast_list_splitting_regex(
       item,
       ~r/\/([[:graph:]].*?[[:graph:]]|[[:graph:]])\//,
       fn [_, content] ->
         {"em", [], [content], %{}}
       end
     )
+    {new_item, acc}
+  end
+  def handle_italics_impl(item, acc), do: {item, acc}
+
+  def handle_bold(ast) do
+    ast
+    |> walk_and_modify_ast(0, &handle_bold_impl/2)
+    |> elem(0)
+  end
+  def handle_bold_impl({"em", attribs, items, annotations}, acc) do
+    {{"strong", attribs, items, annotations}, acc}
+  end
+  def handle_bold_impl(item, acc), do: {item, acc}
+
+  def split_metadata_sections(ast) do
+    walk_and_modify_ast(ast, %{}, fn (item, acc) ->
+      case item do
+        {"pre", [], [{"code", [{"class", "cmsbear-metadata-" <> metadata_id}], [metadata_text], %{}}], %{}} ->
+          metadata = kv_lines_to_map(String.split(metadata_text, "\n"))
+          {[], Map.put(acc, metadata_id, metadata)}
+        text ->
+          {text, acc}
+      end
+    end)
   end
 
-  def process_ast_item({"em", attribs, items, annotations}) do
-    process_ast_item({"strong", attribs, items, annotations})
+  def handle_images_and_figures(ast, metadata_sections) do
+    ast
+    |> walk_and_modify_ast(metadata_sections, &handle_images_and_figures_impl/2)
+    |> elem(0)
   end
-  def process_ast_item({type, attribs, items, annotations})
-  when is_binary(type) and is_list(attribs) and is_list(items) and is_map(annotations) do
-    {type, attribs, List.flatten(process_ast(items)), annotations}
+
+  def use_figcaption_for_title_for_alt(attr) when is_map(attr) do
+    figcaption = Map.get(attr, "figcaption", nil)
+    title = Map.get(attr, "title", nil)
+    alt = Map.get(attr, "alt", nil)
+    title = case {figcaption, title} do
+      {fc, nil} when is_binary(fc) -> fc
+      _ -> title
+    end
+    alt = case {title, alt} do
+      {t, nil} when is_binary(t) -> t
+      _ -> alt
+    end
+    notnil_add_fn = fn (map, key, val) ->
+      case val do
+        nil -> map
+        _ -> Map.put(map, key, val)
+      end
+    end
+    attr |> notnil_add_fn.("title", title) |> notnil_add_fn.("alt", alt)
   end
+
+  def generate_image_attributes(current_attributes, image_opts)
+  when is_map(current_attributes) and is_map(image_opts) do
+    # Title here is used to transmit the metadata section ID, and
+    # should be empty unless the metadata specifies a title.
+    current_attributes = current_attributes
+    |> Map.delete("title")
+    |> Map.delete("alt")
+
+    image_opts
+    |> use_figcaption_for_title_for_alt()
+    |> Enum.into(@default_image_opts)
+    |> Enum.flat_map(
+      fn {key, val} ->
+        case key do
+          "attrib-" <> attrib_name -> [{attrib_name, val}]
+          "title" -> [{"title", val}]
+          "alt" -> [{"alt", val}]
+          _ -> []
+        end
+      end
+    )
+    |> Enum.into(current_attributes)
+  end
+
+  def handle_images_and_figures_impl({"img", attributes, [], %{}} = item, acc) do
+    metadata_sections = acc
+    attributes = Enum.into(attributes, %{})
+    case Map.get(attributes, "title", nil) do
+      nil ->
+        {item, acc}
+      metadata_id ->
+        metadata = Map.get(metadata_sections, metadata_id, %{})
+        new_attributes = generate_image_attributes(attributes, metadata)
+        image = {"img", Map.to_list(new_attributes), [], %{}}
+        case Map.get(metadata, "figcaption", nil) do
+          nil ->
+            {image, acc}
+          figcaption ->
+            caption_id = UUID.uuid4()
+            figure = {
+              "figure", [{"aria-describedby", caption_id}],
+              [image, {"figcaption", [{"id", caption_id}], [figcaption], %{}}],
+              %{}
+            }
+            {figure, acc}
+    #    "<figure aria-describedby='#{id}'>#{image}<figcaption id='#{id}'>#{caption}</figcaption></figure>"
+        end
+    end
+  end
+  def handle_images_and_figures_impl(item, acc), do: {item, acc}
 
   @doc """
   Walks an AST and allows you to process it (storing details in acc) and/or
@@ -119,6 +201,8 @@ defmodule Cmsbear.Markup do
   The process_item_fn function is required. It takes two parameters, the
   single item to process (which will either be a string or a 4-tuple) and
   the accumulator, and returns a tuple {processed_item, updated_acc}.
+  Returning the empty list for processed_item will remove the item processed
+  the AST.
 
   The process_list_fn function is optional and defaults to no modification of
   items or accumulator. It takes two parameters, the list of items that
@@ -132,9 +216,10 @@ defmodule Cmsbear.Markup do
   when is_list(items) and is_function(process_item_fn) and is_function(process_list_fn)
   do
     {items, acc} = process_list_fn.(items, acc)
-    Enum.map_reduce(items, acc, fn (item, acc) ->
+    {ast, acc} = Enum.map_reduce(items, acc, fn (item, acc) ->
       {_item, _acc} = walk_and_modify_ast_item(item, acc, process_item_fn, process_list_fn)
     end)
+    {List.flatten(ast), acc}
   end
 
   def walk_and_modify_ast_item(item, acc, process_item_fn, process_list_fn)
